fc9dcb3ee24b3f70af0281f77e05053385bf9bc5
clinton reynolds <eastwood188209@gmail.com>
2025-04-27 12:20:18 -0400

    fixing configs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "node:path";
+import path from "path";
 import type { Request, Response } from "express";
 // Import the ApolloServer class
 import { ApolloServer } from "@apollo/server";
@@ -8,6 +8,11 @@ import { authenticateToken } from "./services/auth.js"
 // Import the two parts of a GraphQL schema
 import { typeDefs, resolvers } from "./schemas/index.js";
 import db from "./config/connection.js";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
